11591e74ba5e60b89ea9a02698f38cdf89bbdf09
Nokukhanya <lloydkhanyi@gmail.com>
2025-07-12 16:39:18 +0200

    Added python_introduction directory and basic_operation python file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+#Calculating the sum
+sum_result = number1 + number2
+
+#calculating the difference
+subtraction_result = number1 - number2
+
+#multiplying the variables
+product_result = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
+
