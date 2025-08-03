2e60ef7abdb1d407a265073a7f70105d7eedfd45
Nokukhanya <lloydkhanyi@gmail.com>
2025-08-03 16:27:25 +0200

    modified test methods

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,7 +13,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(5, 0), 5)
 
-  def test_subtract(self):
+  def test_subtraction(self):
         """Test the subtract method with various combinations of numbers."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(10, 20), -10)
@@ -21,7 +21,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(0, 0), 0)
         self.assertEqual(self.calc.subtract(-5, -2), -3)
 
-  def test_multiply(self):
+  def test_multiplication(self):
         """Test the multiply method with various combinations of numbers."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(5, 0), 0)
@@ -30,7 +30,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(-2, -3), 6)
         self.assertEqual(self.calc.multiply(2.5, 2), 5.0)
 
-  def test_divide(self):
+  def test_division(self):
         """Test the divide method with various combinations of numbers."""
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
