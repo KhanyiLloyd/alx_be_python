9adabcbb47839c2b10d59e94763be20d4ebc329a
Nokukhanya <lloydkhanyi@gmail.com>
2025-08-10 18:18:52 +0200

    Modified syntax errors in library system python file

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,13 +4,13 @@ class Book:
         self.title=title
         self.author=author
 
-class EBook (Book):
+class EBook(Book):
 
     def __init__(self,title,author,file_size ):
         super().__init__(title, author)
         self.file_size=file_size
 
-class PrintBook (Book):
+class PrintBook(Book):
 
     def __init__(self,title,author,page_count):
         super().__init__(title,author)
